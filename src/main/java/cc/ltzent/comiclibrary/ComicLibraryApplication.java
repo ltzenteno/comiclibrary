992cf7b845a4bc63f9f6fa48dd352ff206376bf9
Luis Zenteno <ltzenteno@gmail.com>
2022-09-05 12:13:04 -0400

    initial commit

---
+++ src/main/java/cc/ltzent/comiclibrary/ComicLibraryApplication.java
@@ -0,0 +1,13 @@
+package cc.ltzent.comiclibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cc/ltzent/comiclibrary/ComiclibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.ltzent.comiclibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComiclibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
